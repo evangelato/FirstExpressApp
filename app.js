796525debcbd60d8e734af6775226e482681d2f9
Evan Hong <evangelurtz@gmail.com>
2019-12-27 01:21:48 +0900

    Add post query with Joi

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -14,6 +17,24 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 })
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
 
 app.get('/api/courses/:id', (req,res) => {
    const course =  courses.find( c => c.id === parseInt(req.params.id));
